f739dfc6846c34e8aa54bd1a5a16edb8b30e6592
Dario Pinto <laradiopotin@gmail.com>
2021-07-29 16:15:29 +0200

    Add err_out for parser + parser test coverage

--- src/parser.ml
+++ src/parser.ml
@@ -1,6 +1,7 @@
 let parse buf =
   try Ok (Menhir_parser.identifier Lexer.token buf) with
   | Lang.Parser_error s -> Error (Format.sprintf "parser error: %s" s)
+  | Menhir_parser.Error -> Error (Format.sprintf "parser error: Syntax error")
   | Lang.Lexer_error s -> Error (Format.sprintf "lexer error: %s" s)
 
 let from_string s = parse (Lexing.from_string s)
