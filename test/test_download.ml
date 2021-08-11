fd8ada3c1791e0bf8db85fd34396d735f9321d0f
Dario Pinto <laradiopotin@gmail.com>
2021-08-11 16:58:45 +0200

    change typing for download, fix test

--- test/test_download.ml
+++ test/test_download.ml
@@ -7,5 +7,6 @@ let () =
   | Ok result ->
     assert (
       result
-      = "https://archive.softwareheritage.org/api/1/content/sha1_git:7bdf38d4468c114206c9b6ebd9cf1176e085d346/raw/" )
+      = Leaf
+          "https://archive.softwareheritage.org/api/1/content/sha1_git:7bdf38d4468c114206c9b6ebd9cf1176e085d346/raw/" )
   | Error _e -> assert false
